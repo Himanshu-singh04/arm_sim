d06f406f44ea350f15402a9b7d0f649fdee89694
Himanshu-singh04 <hsingh_b21@et.vjti.ac.in>
2022-10-24 17:59:33 +0530

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/himanshu04/rover_space/src
+# Build directory: /home/himanshu04/rover_space/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("simulation_rviz")
+subdirs("simulation_gazebo")
---
+++ build/simulation_rviz/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/himanshu04/rover_space/src/simulation_rviz
+# Build directory: /home/himanshu04/rover_space/build/simulation_rviz
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
